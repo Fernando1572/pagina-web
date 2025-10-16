820db8526b91425f07beb96fbafc1f748d7aed7a
Fernando <42302922@uaz.edu.mx>
2025-10-15 22:18:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,85 +5,85 @@
 /*Album 1*/
 /*I saw her standing here*/
 function album1vid1() { //No se te olvide cambiar los links cuando se levante youtube
-    const nuevoLink = "https://youtu.be/oxwAB3SECtc?si=naQW6GejaMLjtKeN"; 
+    const nuevoLink = "https://www.youtube.com/embed/oxwAB3SECtc?si=P1A1MU3YZZVMWYed"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Misery*/
 function album1vid2() {
-    const nuevoLink = "https://youtu.be/qhbcN3ew9z0?si=5OeBw0xRkIOWYZxE"; 
+    const nuevoLink = "https://www.youtube.com/embed/qhbcN3ew9z0?si=UWYeDuf2eRc8tc-8"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Anna (Go To Him)*/
 function album1vid3() {
-    const nuevoLink = "https://youtu.be/b3zNKWyLfus?si=Tmz8ZfVEeKjuexbH";
+    const nuevoLink = "https://www.youtube.com/embed/b3zNKWyLfus?si=n6eFnKPkFLZTEiPG";
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Chains*/
 function album1vid4() {
-    const nuevoLink = "https://youtu.be/rJOhavaeJYk?si=9unXerBZyP7jBfOe"; 
+    const nuevoLink = "https://www.youtube.com/embed/rJOhavaeJYk?si=oTgThdrhr_Cssrj0"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Boys*/
 function album1vid5() {
-    const nuevoLink = "https://youtu.be/Qr8OuW5JJgQ?si=WsBa0GBzFf4ytbcD"; 
+    const nuevoLink = "https://www.youtube.com/embed/Qr8OuW5JJgQ?si=df6E2diY-HUR2AZ6"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Ask Me Why*/
 function album1vid6() {
-    const nuevoLink = "https://youtu.be/2ttGjtfQ7EA?si=TukUOaDZNZZ_iFu8"; 
+    const nuevoLink = "https://www.youtube.com/embed/2ttGjtfQ7EA?si=_I874aTAVsX5mwpO"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Please Please Me*/
 function album1vid7() {
-    const nuevoLink = "https://youtu.be/r5E2fKc8b0M?si=Zk3nYk1eXHjv4m8J"; 
+    const nuevoLink = "https://www.youtube.com/embed/czw8eqepir8?si=EvAp9g_aURLnfoAT"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Love Me Do*/
 function album1vid8() {
-    const nuevoLink = "https://youtu.be/0pGOFX1D_jg?si=7xTJnHZ3X2rYHxZY"; 
+    const nuevoLink = "https://www.youtube.com/embed/0pGOFX1D_jg?si=dyMiHObwFWeTfeUg"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*P.S. I Love You*/
 function album1vid9() {
-    const nuevoLink = "https://youtu.be/MA5DkiVKSlM?si=X796Kxow9AzIjK2Z"; 
+    const nuevoLink = "https://www.youtube.com/embed/MA5DkiVKSlM?si=rFGK5H_3G-FI9fGZ"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Baby It's You*/
 function album1vid10() {
-    const nuevoLink = "https://youtu.be/AWUTlM6hz0g?si=szflAUaHGRZf4aNy";
+    const nuevoLink = "https://www.youtube.com/embed/AWUTlM6hz0g?si=pnHTmz4XOOOeOFYv";
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Do You Want To Know A Secret*/
 function album1vid11() {
-    const nuevoLink = "https://youtu.be/uRQ7ecvU56k?si=at1NS_PXJF-Sn0nh"; 
+    const nuevoLink = "https://www.youtube.com/embed/uRQ7ecvU56k?si=UGEdOscQVuETbg_p"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*A Taste Of Honey*/
 function album1vid12() {
-    const nuevoLink = "https://youtu.be/MkQ1eOcl5ug?si=qcpSp3TEqL2HzB0m"; 
+    const nuevoLink = "https://www.youtube.com/embed/MkQ1eOcl5ug?si=PlVFvWuhF4dVQxNK"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*There's A Place*/
 function album1vid13() {
-    const nuevoLink = "https://youtu.be/vTsbYbN8VVI?si=tBpsvMRzjM9mtJt4"; 
+    const nuevoLink = "https://www.youtube.com/embed/vTsbYbN8VVI?si=84Vo2So-fNBbCGNz"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
 /*Twist And Shout*/
 function album1vid14() {
-    const nuevoLink = "https://youtu.be/2RicaUqd9Hg?si=7xO-fpopw3Ib6paQ"; 
+    const nuevoLink = "https://www.youtube.com/embed/2RicaUqd9Hg?si=1WV5gLEkK8Jiia1a"; 
     document.getElementById("video1").src = nuevoLink;
 }
 
@@ -93,85 +93,85 @@ function album1vid14() {
 
 /*Help!*/
 function album2vid1() {
-    const nuevoLink = "https://youtu.be/MKUex3fci5c?si=wjh9KclLdcdcxEHc"; 
+    const nuevoLink = "https://www.youtube.com/embed/MKUex3fci5c?si=UkmtCOFf1ieYvZ_1"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*The Night Before*/
 function album2vid2() {
-    const nuevoLink = "https://youtu.be/7tD40D7jk-Q?si=Pi8AiupfZHe-IpnB"; 
+    const nuevoLink = "https://www.youtube.com/embed/7tD40D7jk-Q?si=iCsLD2m91Lnnd79x"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*You've Got To Hide Your Love Away*/
 function album2vid3() {
-    const nuevoLink = "https://youtu.be/V8nLraecPRY?si=BIm29uVRqeD5Lwa2"; 
+    const nuevoLink = "https://www.youtube.com/embed/V8nLraecPRY?si=_3XndXMVJT_Mm4Tl"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*I Need You*/
 function album2vid4() {
-    const nuevoLink = "https://youtu.be/6t24nX_sak8?si=KYZ3dQY1eC1TU1XS"; 
+    const nuevoLink = "https://www.youtube.com/embed/6t24nX_sak8?si=s_XwMC1LwHmzsfPV"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*Another Girl*/
 function album2vid5() {
-    const nuevoLink = "https://youtu.be/EMBYNWr5R_U?si=ZwsoFmd9ep2cYPMA"; 
+    const nuevoLink = "https://www.youtube.com/embed/EMBYNWr5R_U?si=95CKPKETApY5jAnw"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*You're Going To Lose That Girl*/
 function album2vid6() {
-    const nuevoLink = "https://youtu.be/vqpEZuv29qE?si=BbxSGQiPKQg9R38U"; 
+    const nuevoLink = "https://www.youtube.com/embed/vqpEZuv29qE?si=740qJDBwOnJUF2dO"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*Ticket To Ride*/
 function album2vid7() {
-    const nuevoLink = "https://youtu.be/SyNt5zm3U_M?si=OykrSewN4MF72deH"; 
+    const nuevoLink = "https://www.youtube.com/embed/SyNt5zm3U_M?si=RN6xDuV2OrYwAw6I"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*Act Naturally*/
 function album2vid8() {
-    const nuevoLink = "https://youtu.be/yjJd8rZPcAQ?si=mHSp4KJk4R2fTVe1"; 
+    const nuevoLink = "https://www.youtube.com/embed/yjJd8rZPcAQ?si=TEFr6nKYoB8L-fcJ"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*It's Only Love*/
 function album2vid9() {
-    const nuevoLink = "https://youtu.be/OPiKDHYCkjs?si=qsLyOPJT0wEtWPhS"; 
+    const nuevoLink = "https://www.youtube.com/embed/OPiKDHYCkjs?si=RO8foMpx_8vTfV_y"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*You Like Me Too Much*/
 function album2vid10() {
-    const nuevoLink = "https://youtu.be/ZqCf3D26YlA?si=4LdVOlj6BgLQD8uG"; 
+    const nuevoLink = "https://www.youtube.com/embed/ZqCf3D26YlA?si=DDUjXj_pBEmS-pVb"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*Tell Me What You See*/
 function album2vid11() {
-    const nuevoLink = "https://youtu.be/bztiAcsATyI?si=4eX9zDL6AkIDgHFo"; 
+    const nuevoLink = "https://www.youtube.com/embed/bztiAcsATyI?si=BAIsbNiUM9i_sEeb"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*I've Just Seen a Face*/
 function album2vid12() {
-    const nuevoLink = "https://youtu.be/m8LbJfC0SYM?si=PEWbHC2kHkzHJ_AH"; 
+    const nuevoLink = "https://www.youtube.com/embed/m8LbJfC0SYM?si=wGY0IVA1k0mVXmJ1"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*Yesterday*/
 function album2vid13() {
-    const nuevoLink = "https://youtu.be/NrgmdOz227I?si=-nI5BiamcovjFijL"; 
+    const nuevoLink = "https://www.youtube.com/embed/NrgmdOz227I?si=tpGr5LjyJ3b_Tx83"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
 /*Dizzy Miss Lizzle*/
 function album2vid14() {
-    const nuevoLink = "https://youtu.be/psJ1cHm_su4?si=0lQrK7qLkTmgtf0D"; 
+    const nuevoLink = "https://www.youtube.com/embed/psJ1cHm_su4?si=_9DMCKaCGCjkOFi2"; 
     document.getElementById("video2").src = nuevoLink;
 }
 
